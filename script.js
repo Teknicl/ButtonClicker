186ea60cdd2195fc6b60f54129c184c37dd95e35
Torrei Whitfield <bossbollin@msn.com>
2023-01-11 01:24:43 -0800

    Completed assignment

---
+++ script.js
@@ -0,0 +1,15 @@
+function logOut(element) {
+    if(element.innerText == "Login") {
+        element.innerText = "Logout";
+    } else {
+        element.innerText = "Login";
+    }   
+}
+
+function hide(element) {
+    element.remove();
+}
+
+function message() {
+    alert("Ninja was liked!");
+}
